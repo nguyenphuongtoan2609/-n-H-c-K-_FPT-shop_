5261add0f5fa48646fe38f3e70017ef529bf90a5
NGUYỄN PHƯƠNG TOÀN <22dh114769@st.huflit.edu.vn>
2023-10-09 23:17:20 +0700

    Add project files.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Đề_Án_Học_Kỳ__TGDD_
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
